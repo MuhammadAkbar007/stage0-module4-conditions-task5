8df77770079dc8915daac4e1419133a00594d094
MuhammadAkbar007 <fotismenos007@mail.ru>
2024-03-29 16:04:56 +0500

    Bismillah 🥇

--- src/main/java/school/mjc/stage0/conditions/task5/RangeNumbersPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task5/RangeNumbersPrinter.java
@@ -2,5 +2,21 @@ package school.mjc.stage0.conditions.task5;
 
 public class RangeNumbersPrinter {
     public void printRange(int number) {
+        switch (number) {
+            case 1:
+            case 2:
+            case 3:
+            case 4:
+            case 5:
+                System.out.println(number);
+                break;
+            case 6:
+            case 7:
+            case 8:
+            case 9:
+            case 10:
+                System.out.println("out of range");
+                break;
+        }
     }
 }
